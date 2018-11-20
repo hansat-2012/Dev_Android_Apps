a9ed37bc8eea814de660c46b19451f6a986ef367
Akira Toda <todayo@aktoda.local>
2018-11-20 22:19:17 +0900

    Function class draft

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070021;
-        public static final int end = 0x7f070036;
-        public static final int gone = 0x7f07003d;
-        public static final int invisible = 0x7f070047;
-        public static final int left = 0x7f070049;
-        public static final int packed = 0x7f070057;
-        public static final int parent = 0x7f070058;
-        public static final int percent = 0x7f07005a;
-        public static final int right = 0x7f07005e;
-        public static final int spread = 0x7f070076;
-        public static final int spread_inside = 0x7f070077;
-        public static final int start = 0x7f07007c;
-        public static final int top = 0x7f07008b;
-        public static final int wrap = 0x7f070091;
+        public static final int end = 0x7f070046;
+        public static final int gone = 0x7f07004d;
+        public static final int invisible = 0x7f070057;
+        public static final int left = 0x7f070059;
+        public static final int packed = 0x7f070067;
+        public static final int parent = 0x7f070068;
+        public static final int percent = 0x7f07006a;
+        public static final int right = 0x7f07006e;
+        public static final int spread = 0x7f070086;
+        public static final int spread_inside = 0x7f070087;
+        public static final int start = 0x7f07008c;
+        public static final int top = 0x7f07009d;
+        public static final int wrap = 0x7f0700a3;
     }
     public static final class styleable {
         private styleable() {}
---
+++ app/src/main/java/com/example/root/myapplication/Function.java
@@ -0,0 +1,53 @@
+package com.example.root.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/*
+*   途中計算式を表示するクラス
+ */
+
+public class Function extends AppCompatActivity {
+    private String m_formula = null;
+    private boolean m_bCalcLast = false;     //直近の入力が演算式である
+    private static final int LIMIT = 20;    // Function欄の最大文字数を20文字とする
+
+    public void setNum(int num) {
+        if(num < 0 || 9 < num ) {   //無効な文字が来た場合
+            return;
+        }
+        if( LIMIT < m_formula.length()){    //表示しきれない場合
+            return;
+        }
+        m_formula += num;
+        m_bCalcLast = false;
+        Redraw();
+    }
+
+    /*
+    * 0 : +
+    * 1 : -
+    * 2 : *
+    * 3 : /
+     */
+    public void setCalc(int calc) {
+        if(calc < 0 || 4 < calc) {
+            return;
+        }
+        if( LIMIT < m_formula.length()){
+            return;
+        }
+        //演算子が２回続いた場合は更新する
+        if(m_bCalcLast) {
+            m_formula = m_formula.substring(0,m_formula.length()-1);
+        }
+        m_formula += calc;
+        m_bCalcLast = true;
+        Redraw();
+    }
+
+    private void Redraw(){
+        TextView display = findViewById(R.id.textView4);
+        display.setText(m_formula);
+    }
+}
