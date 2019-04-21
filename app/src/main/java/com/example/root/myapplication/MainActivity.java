f2f3c9f3c8e0e1e27b6e0e66c2648827c15eed67
todayo <todayo1212@gmail.com>
2019-04-21 21:15:26 +0900

    MainActivity:文字数制限追加。ひとまず9桁まで。

--- app/src/main/java/com/example/root/myapplication/MainActivity.java
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -204,6 +204,9 @@ public class MainActivity extends Activity {
             m_dispStr = ""; //最初から表示されてる0を消したいので一旦クリア
             m_bZero = false;
         }
+        if(10 < m_dispStr.length()) {
+            return; //字数制限。ひとまず9桁まで
+        }
         showNum(num);
         m_func.setNum(num);
         m_buf.saveToken(num);
